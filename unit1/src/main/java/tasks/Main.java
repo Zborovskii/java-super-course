321d93f6514990edb9a2021a3ca1a6591764d7b4
Alexander <shum-127127@mail.ru>
2020-01-25 15:36:08 +0300

    second commite

--- unit1/src/main/java/tasks/Main.java
+++ unit1/src/main/java/tasks/Main.java
@@ -1,48 +1,52 @@
 package tasks;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         //Task 2. Start
         Integer[] numbers = new Integer[]{1, 2, 3, 4, 5, 6};
-        Integer[] result = new Integer[9];
         //cycle for
-        for (Integer i = 0; i < numbers.length; i++) {
-            if ((i - 1) % 2 == 0) {
-                result[i] = numbers[i] * numbers[i - 1];
-            } else {
-                result[i] = numbers[i];
-            }
+
+        for (int i = 1; i < numbers.length; i += 2) {
+            numbers[i] = numbers[i] * numbers[i - 1];
         }
+
 
         //cycle do while
-        Integer i = 0;
+        Integer[] numbers1 = new Integer[]{1, 2, 3, 4, 5, 6};
+
+        Integer i = 1;
         do {
-            if ((i - 1) % 2 == 0) {
-                result[i] = numbers[i] * numbers[i - 1];
-            } else {
-                result[i] = numbers[i];
-            }
-            i++;
+            numbers1[i] = numbers1[i] * numbers1[i - 1];
+
+            i += 2;
         }
-        while (i < numbers.length);
+        while (i < numbers1.length);
+
 
         //cycle while
-        i = 0;
-        while (i < numbers.length) {
-            if ((i - 1) % 2 == 0) {
-                result[i] = numbers[i] * numbers[i - 1];
-            } else {
-                result[i] = numbers[i];
-            }
-            i++;
+        Integer[] numbers2 = new Integer[]{1, 2, 3, 4, 5, 6};
+
+        i = 1;
+        while (i < numbers2.length) {
+            numbers2[i] = numbers2[i] * numbers2[i - 1];
+
+            i += 2;
         }
 
         //Task 2. finish
 
+        //Task 3
+        calculateFunction(1,6,2);
+
         //Task 4
-        Main.getMax(new int[]{1,2,3,4});
+        Arrays.toString(Main.getMax(new int[]{1, 2, 3, 10, 5 ,20}));
+
+        //Task 5
+        printMatrix(5);
     }
 
     //Task 3
@@ -50,40 +54,36 @@ public class Main {
 
         System.out.println(" x | y \n");
 
-        for (int i = a; b > i * h; i++) {
+        for (int i = a; b >= i ; i+=h) {
             System.out.println(String.format(" %s | %s ", i, Math.tan(2 * i) - 3));
         }
     }
 
     //Task 4 method
     private static Integer[] getMax(int[] arr) {
-        int pairQuantity = arr.length / 2;
+        int pairQuantity = arr.length / 4;
         Integer[] result = new Integer[pairQuantity];
 
         for (int i = 1; i <= pairQuantity; i++) {
-            result[i - 1] = Math.max(arr[i - 1], arr[arr.length - i]);
+            result[i - 1] = Math.max(arr[i - 1] + arr[arr.length - i], arr[i] + arr[arr.length - 1 - i]);
         }
-
         return result;
     }
 
     //Task5
-
     public static void printMatrix(Integer size) {
-        StringBuilder result = new StringBuilder();
+        int[][] result = new int[size][size];
 
-        for (Integer i = 0; i < size; i++) {
-            for (Integer j = 0; j < size; j++) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 if (i == j || i + j == size - 1) {
-                    result.append("1");
+                    result[i][j] = 1;
                 } else {
-                    result.append("0");
+                    result[i][j] = 0;
                 }
             }
-            result.append("\n");
+            System.out.println(Arrays.toString(result[i]));
         }
-
-        System.out.println(result);
     }
 
 }
